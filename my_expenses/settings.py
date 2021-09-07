1bd1a737dc022b6476633a05db5ce0b037b4dd69
Danilo Miranda <danilo030920@gmail.com>
2021-09-07 13:18:59 -0300

    fix staticfiles

--- my_expenses/settings.py
+++ my_expenses/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-jgn!yz%ryo-r+3#dg*h$j%2z8s+mz38z^&b!pi+1uu3-ypj1em
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://api-my-expenses.herokuapp.com/"]
 
 
 # Application definition
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
